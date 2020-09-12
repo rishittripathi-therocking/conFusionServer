61edbcecc3724e0211723cdfa5c3a0baddde38c5
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-09-12 10:07:59 +0530

    Cookies

--- app.js
+++ app.js
@@ -30,35 +30,48 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-98765'));
 
 function auth(req,res,next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
 
-  var authHeader = req.headers.authorization;
+    if(!authHeader) {
+      var err = new Error('You are not authenticated');
 
-  if(!authHeader) {
-    var err = new Error('You are not authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
+    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+    var username= auth[0];
+    var password = auth[1];
 
-  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
-  var username= auth[0];
-  var password = auth[1];
+    if(username === 'admin' && password === 'password') {
+      res.cookie('user','admin',{ signed: true });
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated');
 
-  if(username === 'admin' && password === 'password') {
-    next();
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
   else {
-    var err = new Error('You are not authenticated');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if(req.signedCookies.user === 'admin'){
+      next();
+    }
+    else  {
+      var err = new Error('You are not authenticated');
+      err.status = 401;
+      return next(err);
+    }
   }
+  
 }
 
 app.use(auth);

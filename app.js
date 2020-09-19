22fcf62d0ab7b2a643a80b958b2443b4fb0e2ee2
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-09-19 15:20:41 +0530

    image Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishesRouter = require('./routes/dishRouters');
 var promoRouter = require('./routes/promoRouters');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 
 const mongoose = require('mongoose');
@@ -60,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishesRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 // catch 404 and forward to error handler
